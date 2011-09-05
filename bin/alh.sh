753870002b9e4595401061867be2ae1e41f2e1f0
Sebastian Schmidt <yath@yath.de>
2011-09-05 16:46:31 +0200

    Fix L10N_HELPER_HOME resolution
    Use readlink -f to determine path to alh.sh to make it work if it's
    called via $PATH resolution.

--- bin/alh.sh
+++ bin/alh.sh
@@ -2,7 +2,6 @@
 #
 # Start-up script
 
-L10N_HELPER_HOME=`dirname "$0"`
-L10N_HELPER_HOME=`dirname "$L10N_HELPER_HOME"`
+L10N_HELPER_HOME="$(dirname "$(readlink -f "$0")")/.."
 
 java -jar $L10N_HELPER_HOME/lib/l10n-helper.jar $1
